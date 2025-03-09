fa7474b9b77fd3af7f0f0c4c38a1d07ea4ab3acf
AhmedSaleh74 <AhmedSaleh1112001@gmail.com>
2025-03-09 21:25:19 +0200

    Enhanced UI: Added gradient background, improved quote display, and animated button.

--- lib/screens/quote_home.dart
+++ lib/screens/quote_home.dart
@@ -9,32 +9,86 @@ class QuoteHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("اقتباسات ملهمة"),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Obx(() => Text(
-                    controller.currentQuote.value.quoteText,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                  )),
-              SizedBox(height: 10),
-              Obx(() => Text(
-                    "- ${controller.currentQuote.value.author}",
-                    style: TextStyle(fontSize: 18, fontStyle: FontStyle.italic),
-                  )),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: controller.changeQuote,
-                child: Text("اقتباس جديد"),
-              ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.deepPurple,
+              Colors.purpleAccent,
+              Colors.pinkAccent,
             ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Obx(() => Container(
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.2),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Column(
+                        children: [
+                          Text(
+                            controller.currentQuote.value.quoteText,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            controller.currentQuote.value.author,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontStyle: FontStyle.italic,
+                              color: Colors.white70,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )),
+                SizedBox(height: 30),
+                GestureDetector(
+                  onTap: controller.changeQuote,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [Colors.orange, Colors.deepOrange],
+                      ),
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 10,
+                          spreadRadius: 2,
+                          offset: Offset(0, 5),
+                        ),
+                      ],
+                    ),
+                    child: Text(
+                      "اقتباس جديد",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
